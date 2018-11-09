c4a80912d113ad214aaf6b093f40a999afc3db1c
Kevin <kevinxela13@gmail.com>
2018-11-09 13:55:08 -0600

    Publicaciones como borrador

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.listar),
+    url(r'^$', views.listar, name='listar'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
-            post.fecha_publicacion = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -34,9 +33,22 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
-            post.fecha_publicacion = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Publicar.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Publicar, pk=pk)
+    post.publicar()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Publicar, pk=pk)
+    post.delete()
+    return redirect('listar')
