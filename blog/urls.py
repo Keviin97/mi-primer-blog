ba5463eeb280de9c1cb24a87d08ebe695b050922
Kevin <kevinxela13@gmail.com>
2018-11-09 13:04:50 -0600

    Vista de detalle

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.listar),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicar
+from django.shortcuts import render, get_object_or_404
+
 
 def listar(request):
     articulos = Publicar.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {'articulos':articulos})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Publicar, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
